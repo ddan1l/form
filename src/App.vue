b659cafa307608e0187df646214793fbfd858d5a
ddan1l <68752884+ddan1l@users.noreply.github.com>
2021-05-29 22:29:47 +0300

    'create-from'

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,10 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <router-view></router-view>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
@@ -25,4 +17,8 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+body, .container{
+  min-height: 100vh;
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from "./router";
+import VueRouter from "vue-router";
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import firebaseConfig from './config/firebase'
+import firebase from "firebase";
+import "firebase/firestore"
 
 Vue.config.productionTip = false
 
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.use(VueRouter)
+
+firebase.initializeApp(firebaseConfig);
+
+
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
